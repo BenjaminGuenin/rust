5ec5087879b92dc9758cc618e00b0f37f586770f
Ethan <ethan@no-reply.com>
2018-07-07 19:06:24 +0200

    Borrowing error for a mutable reference

--- rust-programming-tutorial/src/main.rs
+++ rust-programming-tutorial/src/main.rs
@@ -3,10 +3,9 @@ fn main() {
     let mut x = 10;
 
     let dom = &mut x;
-
     *dom += 1;
+    println!("x is {}", x);
 
-    println!("x is {}", dom);
-
+    // We can't define a mutable reference to x and borrow it at the same time.
 
 }
