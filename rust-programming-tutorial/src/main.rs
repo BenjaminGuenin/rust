1b7ed47a234c762b179379f464f90c5d192dc3c2
Ethan <ethan@no-reply.com>
2018-07-07 19:30:56 +0200

    Solution to borrowing mutable reference

--- rust-programming-tutorial/src/main.rs
+++ rust-programming-tutorial/src/main.rs
@@ -2,8 +2,11 @@ fn main() {
 
     let mut x = 10;
 
-    let dom = &mut x;
-    *dom += 1;
+    {
+        let dom = &mut x;
+        *dom += 1;
+    }
+    
     println!("x is {}", x);
 
     // We can't define a mutable reference to x and borrow it at the same time.
