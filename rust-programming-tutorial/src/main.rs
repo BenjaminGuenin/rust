143ae16469dcd3a10cab71757473fa6eb60d9bca
Ethan <ethan@no-reply.com>
2018-07-07 18:48:44 +0200

    test

---
+++ rust-programming-tutorial/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut x = 10;
+    let xr = &x;
+    let dom = &x;
+    
+    // I am making some changes
+
+    println!("x is {}", x);
+    println!("x is {}", xr);
+    println!("x is {}", dom);
+
+}
