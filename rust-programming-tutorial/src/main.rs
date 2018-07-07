d34aae8355aa97c1156eaac119a9094962cfae62
Ethan <ethan@no-reply.com>
2018-07-07 19:01:35 +0200

    Mutable reference

--- rust-programming-tutorial/src/main.rs
+++ rust-programming-tutorial/src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
-    let mut x = 10;
-    let xr = &x;
-    let dom = &x;
-    
-    dom +=1; // Will not work because the reference is not mutable.
 
-    println!("x is {}", x);
-    println!("x is {}", xr);
+    let mut x = 10;
+
+    let dom = &mut x;
+
+    *dom += 1;
+
     println!("x is {}", dom);
+
 
 }
