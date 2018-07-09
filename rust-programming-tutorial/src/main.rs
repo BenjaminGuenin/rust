bda9cf0d16c5526f7bd5a31f818617df852b7f2a
Ethan <ethan@no-reply.com>
2018-07-09 08:16:02 +0200

    Need to define a struct as mutable

--- rust-programming-tutorial/src/main.rs
+++ rust-programming-tutorial/src/main.rs
@@ -1,13 +1,9 @@
 #[derive(Debug)]
-struct Color {
-    red: u8, // u8: 0 to 255
-    green: u8,
-    blue: u8,
-}
+struct Color (u8, u8, u8);
 
 fn main() {
-    let mut bg = Color {red: 255, green: 70, blue: 15};
-    print!("color is {}", bg.blue);
-    bg.blue = 45; // impossible, because we need an mutable struct.
-    print!("color is {}", bg.blue);
+    let red = Color(255,0,0);
+    println!("red is {}, {}, {}", red.0, red.1, red.2);
+    red.2 = 98;
+    println!("red is {}, {}, {}", red.0, red.1, red.2);
 }
