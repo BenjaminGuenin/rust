c94628178ba3871fc386d61430d4c0759ccfeebe
Ethan <ethan@no-reply.com>
2018-07-09 08:17:13 +0200

    Mutable struct

--- rust-programming-tutorial/src/main.rs
+++ rust-programming-tutorial/src/main.rs
@@ -2,7 +2,7 @@
 struct Color (u8, u8, u8);
 
 fn main() {
-    let red = Color(255,0,0);
+    let mut red = Color(255,0,0);
     println!("red is {}, {}, {}", red.0, red.1, red.2);
     red.2 = 98;
     println!("red is {}, {}, {}", red.0, red.1, red.2);
