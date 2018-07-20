ca173ca95d1920b15a8f4c06ed87084cdf89e670
Ethan <ethan@no-reply.com>
2018-07-20 08:21:17 +0200

    clean main.rs

---
+++ rust-programming-tutorial/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+
+    let immutable_string = "hello"; // cannot be modified - stored on the stack
+    let mut mutable_string = String::from("hello"); // can be modified - stored on the heap
+
+    mutable_string.push_str(" world!");
+    println!("{}", mutable_string);
+
+    // Scope ownership memory management
+    {
+        let s = String::from("hello"); // s is valid from this point forward
+
+        // do stuff with s
+    }   // this scope is now over, and s is no longer valid. The memory taken by s is fred.
+
+}
