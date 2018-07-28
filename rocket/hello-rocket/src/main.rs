132c3efcab968b8ff654df2a163597b77403f422
Starli0n <Starli0n@users.noreply.github.com>
2018-07-28 18:31:31 +0200

    OnGoing Setting up DB connection

--- rocket/hello-rocket/src/main.rs
+++ rocket/hello-rocket/src/main.rs
@@ -1,13 +1,62 @@
 #![feature(plugin)]
 #![plugin(rocket_codegen)]
+#[macro_use] extern crate diesel;
 
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hey friends!!"
+use std::ops::Deref;
+use rocket::http::Status;
+use rocket::request::{self, FromRequest};
+use rocket::{Request, State, Outcome};
+use diesel::sqlite::SqliteConnection;
+use diesel::r2d2::{ConnectionManager, Pool, PooledConnection};
+
+// An alias to the type for a pool of Diesel SQLite connections.
+type SqlitePool = Pool<ConnectionManager<SqliteConnection>>;
+
+// The URL to the database, set via the `DATABASE_URL` environment variable.
+static DATABASE_URL: &'static str = "localhost:3306";
+
+/// Initializes a database pool.
+fn init_pool() -> SqlitePool {
+    let manager = ConnectionManager::<SqliteConnection>::new(DATABASE_URL);
+    Pool::new(manager).expect("db pool")
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .manage(init_pool())
+        .launch();
+}
+
+
+// Connection request guard type: a wrapper around an r2d2 pooled connection.
+pub struct DbConn(pub PooledConnection<ConnectionManager<SqliteConnection>>);
+
+/// Attempts to retrieve a single connection from the managed database pool. If
+/// no pool is currently managed, fails with an `InternalServerError` status. If
+/// no connections are available, fails with a `ServiceUnavailable` status.
+impl<'a, 'r> FromRequest<'a, 'r> for DbConn {
+    type Error = ();
+    fn from_request(request: &'a Request<'r>) -> request::Outcome<Self, Self::Error> {
+        let pool = request.guard::<State<SqlitePool>>()?;
+        match pool.get() {
+            Ok(conn) => Outcome::Success(DbConn(conn)),
+            Err(_) => Outcome::Failure((Status::ServiceUnavailable, ()))
+        }
+    }
+}
+
+// For the convenience of using an &DbConn as an &SqliteConnection.
+impl Deref for DbConn {
+    type Target = SqliteConnection;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[get("/test")]
+fn index() -> &'static str {
+    "Hey friends!!"
 }
